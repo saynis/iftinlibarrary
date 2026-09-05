0b5f2c254614aba7e5a7c3256d749e47af21d03a
saynis <aishaabdifetah@gmail.com>
2026-09-05 18:37:07 +0300

    resposive added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom"
 import { FaCartArrowDown } from "react-icons/fa6";
-import { useContext } from "react";
-import { UserContext } from "../UserContext";
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 
 
 function Header() {
 
-  const {user} = useContext(UserContext)
 
-
+  const [open, setOpen] = useState(false)
 
 
   return (
@@ -25,45 +25,85 @@ function Header() {
             <img className="h-16" width={50} src="/jju.png" alt="logo.png" />
           </div>
 
-          {/* naviagtion */}
-          <nav className="flex">
 
-            <ul className="flex gap-8 text-xl font-semibold text-gray-700">
-              <li>
-                <Link to="/" >Home</Link>
-              </li>
+          {/* responsive div */}
+          <div className={`md:flex
+        items-center gap-24 
+        text-center
+        absolute md:static 
+        right-0  bg-[#E3DCC4] 
+        h-screen md:h-auto w-[50%] md:w-auto
+       ${open ? "  top-24" : "top-10000"}
+       
+        `} >
 
-               <li>
-                <Link to="/books" >Books</Link>
-              </li>
+            {/* naviagtion */}
+            <nav className="">
 
-               <li>
-                <Link to="/about" >About</Link>
-              </li>
+              <ul className="md:flex gap-8 space-y-4 text-center
+                text-xl font-semibold text-gray-700">
+                <li onClick={() => setOpen(false)}>
+                  <Link to="/" >Home</Link>
+                </li>
 
-               <li>
-                <Link to="/contact" >Contact</Link>
-              </li>
-            </ul>
+                <li onClick={() => setOpen(false)}>
+                  <Link to="/books" >Books</Link>
+                </li>
 
-          </nav>
+                <li onClick={() => setOpen(false)}>
+                  <Link to="/about" >About</Link>
+                </li>
+
+                <li onClick={() => setOpen(false)}>
+                  <Link to="/contact" >Contact</Link>
+                </li>
+              </ul>
+
+            </nav>
 
 
-          {/* buttons div */}
+            {/* buttons div */}
 
-          <div className="flex gap-8 items-center">
-            <button className="text-xl font-bold">Login</button>
-            <button className="py-2 
+            <div className="md:flex-row flex flex-col  md:gap-8 gap-4 mx-auto ">
+              <button className="text-xl font-bold md:mx-auto mx-0">Login</button>
+              <button className="py-2  md:mx-auto mx-0
             px-6 bg-[#2A7EFF] text-xl rounded-md text-white
             ">Sign up</button>
 
-           <Link to="/cart"><FaCartArrowDown className="text-3xl text-[#2A73FF] cursor-pointer" /></Link> 
-           <span>{user}</span>
+            </div>
+
+
+
+
+
+
+
+
+
           </div>
 
-         
-      
-          
+
+        {/* menu and cart */}
+          <div className="flex gap-6 items-center">
+            <Link
+              to="/cart">
+              <FaCartArrowDown
+                className="text-3xl text-[#2A73FF] cursor-pointer" />
+            </Link>
+
+            {open ? (
+              <IoMdClose
+                onClick={() => setOpen(!open)}
+                className="text-4xl md:hidden cursor-pointer" />
+            ) : (
+              <IoMdMenu
+                onClick={() => setOpen(!open)}
+                className="text-4xl md:hidden cursor-pointer" />
+            )}
+
+          </div>
+
+
         </div>
 
       </div>
